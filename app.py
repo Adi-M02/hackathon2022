039c3f62b4d7f37693666c518cfd19c03ed64f4c
Amol Bhagavathi <abhagavathi21@gmail.com>
2022-10-15 15:56:54 -0500

    BeautifulSoup to Parse Index.html

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 from flask import Flask, render_template
+from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+with open("./templates/index.html","r") as f:
+    doc =BeautifulSoup(f,"html.parser")
+
+print(doc.prettify())
