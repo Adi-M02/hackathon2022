42260c28ac432b361fe1f71ff3f33c8760c36106
Amol Bhagavathi <abhagavathi21@gmail.com>
2022-10-15 16:51:58 -0500

    Added More to App.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from bs4 import BeautifulSoup
+import requests
 
 app = Flask(__name__)
 
@@ -7,7 +8,10 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-with open("./templates/index.html","r") as f:
-    doc =BeautifulSoup(f,"html.parser")
 
-print(doc.prettify())
+url="https://www.newegg.com/msi-geforce-rtx-3080-rtx-3080-ventus-3x-plus-12g-oc-lhr/p/N82E16814137712?quicklink=true"
+result=requests.get(url)
+doc = BeautifulSoup(result.text,"html.parser")
+
+prices=doc.find_all(text="$")
+print(prices)
